263348618344a1bde28d8575d36681c63eae2405
Banja David Lotigo <innocdavid@gmail.com>
2023-05-25 11:31:48 +0300

    Add a function that creates new todo

--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from .forms import TodoForm
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'app/todos.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'app/todo_create.html', {'form': form})
